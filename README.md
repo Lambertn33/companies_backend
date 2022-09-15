65906571e76f3abc97f326dd9bd70f272587a5a5
Lambertn33 <lambertgithub@gmail.com>
2022-09-15 22:10:01 +0200

    feat: add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Steps To Run the Project
+
+1. clone the project
+2. run npm i 
+3. npm start
--- routes/data.js
+++ routes/data.js
@@ -1,57 +1,57 @@
  const Companies = [
         {
             'id': 1,
-            'name': "Company 1",
+            'name': "Africa XYZ",
             'speciality':'Excavation',
-            'City': 'Singapore'
+            'city': 'Singapore'
         },
         {
             'id': 2,
-            'name': "Company 2",
+            'name': "Company from Vatican",
             'speciality':'Plumbing',
-            'City': 'Vatican'
+            'city': 'Vatican'
         },
         {
             'id': 3,
-            'name': "Company 3",
+            'name': "Engineering Company",
             'speciality':'Electrical',
-            'City': 'Dakar'
+            'city': 'Dakar'
         },
         {
             'id': 4,
             'name': "Company 4",
             'speciality':'Plumbing',
-            'City': 'New York'
+            'city': 'New York'
         },
         {
             'id': 5,
             'name': "Company 5",
             'speciality':'Excavation',
-            'City': 'Singapore'
+            'city': 'Singapore'
         },
         {
             'id': 6,
-            'name': "Company 6",
+            'name': "DEXF",
             'speciality':'Electrical',
-            'City': 'Vatican'
+            'city': 'Vatican'
         },
         {
             'id': 7,
-            'name': "Company 7",
+            'name': "REG",
             'speciality':'Electrical',
-            'City': 'Dakar'
+            'city': 'Dakar'
         },
         {
             'id': 8,
-            'name': "Company 8",
+            'name': "T-IT AFRICA",
             'speciality':'Plumbing',
-            'City': 'New York'
+            'city': 'New York'
         },
         {
             'id': 9,
-            'name': "Company 9",
+            'name': "Aegis Consult Ltd",
             'speciality':'Excavation',
-            'City': 'New York'
+            'city': 'New York'
         },
  ]
  exports.companies = Companies
